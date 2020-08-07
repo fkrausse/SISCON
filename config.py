a861b8c66681f5aca9cc8b904db2d91195c8ab00
Fabian Adolfo <fabi_adolfo@hotmail.com>
2020-08-07 00:16:29 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ MYSQL_HOST = "us-cdbr-east-02.cleardb.com"
 MYSQL_USER = "bffef14ac89b72"
 MYSQL_PASSWORD = "3f269b60"
 MYSQL_DB = "heroku_c297a0dd5b30859"
-MYSQL_PORT = 3306
+MYSQL_PORT = 5000
 
 #UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
